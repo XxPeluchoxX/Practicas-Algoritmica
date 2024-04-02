434891cd387f7e10b0a62c3f191d3e9eec9d83d4
Arturo Olivares Martos <2004.olivares@gmail.com>
2024-04-02 19:35:07 +0200

    programa1 terminado

--- P2/programa1/Subsecuencia_DyV.cpp
+++ P2/programa1/Subsecuencia_DyV.cpp
@@ -19,122 +19,132 @@ using namespace std;
 int suma(const vector<int> & v, int init, int fin){
     int result=0;
 
-    for(int i=init; i<=fin; i++)
-        result+=v[i];
+    bool index_ok = 0<=init && init<=fin && fin<v.size();
+
+    // Tan solo se calcula la suma si los indices son correctos
+    // Evitamos así las subsecuencias vacias
+    if(index_ok)
+        for(int i=init; i<=fin; ++i)
+            result+=v[i];
     
     return result;
 }
 
 
+
 /**
- * @brief Funcion que calcula la subsecuencia maxima de un vector de enteros.
+ * @brief Funcion que calcula la subsecuencia maxima de un vector de enteros. Algoritmo Divide y Venceras.
  * 
  * @param v  Vector de enteros
  * @param init  Posicion inicial
  * @param fin  Posicion final
  * @return pair<int,int>  Par de enteros que representan la posicion inicial y final de la subsecuencia maxima
  */
-pair<int,int> subsecMax (const vector<int> &v, int init, int fin){
+pair<int,int> subsecMax_DyV (const vector<int> &v, int init, int fin){
         
-    pair<int,int> result;
-    int suma_result;
+    pair<int,int> bounds_max;
+    int  suma_max;
     
     // Caso base. n=1
     if(fin <= init){
-        result.first=result.second=init;
-        return result;
+        bounds_max.first=bounds_max.second=init;
+        suma_max=v[init];
     }
+        
 
     else{
         int middle=init + (fin-init)/2;
 
-        pair<int,int> result_izq  = subsecMax(v,init,middle);   // Subsecuencia maxima de la izquierda
-        pair<int,int> result_dcha = subsecMax(v,middle+1,fin);  // Subsecuencia maxima de la derecha
+        pair<int,int> bounds_max_izq  = subsecMax_DyV(v,init,middle);   // Subsecuencia maxima de la izquierda
+        pair<int,int> bounds_max_dcha = subsecMax_DyV(v,middle+1,fin);  // Subsecuencia maxima de la derecha
 
-        int suma_izq  = suma(v,result_izq.first,result_izq.second);
-        int suma_dcha = suma(v,result_dcha.first,result_dcha.second);
+        int suma_max_izq  = suma(v,bounds_max_izq.first,bounds_max_izq.second);
+        int suma_max_dcha = suma(v,bounds_max_dcha.first,bounds_max_dcha.second);
 
 
         // Combinacion del centro
-		// Bucamos la subsecMax que contenga a middle y a middle+1
-        pair <int,int> result_central (middle,middle+1);
-		int sum_max = suma(v, result_central.first, result_central.second);
+		// Bucamos la subsecMax_DyV que contenga a middle y a middle+1
+        pair <int,int> bounds_max_central (middle,middle+1);
+		int suma_max_central = suma(v, bounds_max_central.first, bounds_max_central.second);
 
-		int sum_tmp = sum_max;		
+		int suma_central_tmp = suma_max_central;		
 		// Actualizo el borde por la izquierda.
-		// Voy desplazandome a la izquierda (hasta encontrarme con result_izq) y actualizando la suma
+		// Voy desplazandome a la izquierda (hasta encontrarme con bounds_max_izq) y actualizando la suma
         int lim_izq_tmp;
-		for (lim_izq_tmp=result_central.first-1; lim_izq_tmp>result_izq.second; lim_izq_tmp--){
-			sum_tmp += v[lim_izq_tmp];
-			if (sum_tmp>=sum_max){
-				result_central.first = lim_izq_tmp;
-				sum_max = sum_tmp;
+		for (lim_izq_tmp=bounds_max_central.first-1; lim_izq_tmp>bounds_max_izq.second; lim_izq_tmp--){
+			suma_central_tmp += v[lim_izq_tmp];
+			if (suma_central_tmp>=suma_max_central){
+				bounds_max_central.first = lim_izq_tmp;
+				suma_max_central = suma_central_tmp;
 			}
 		}
-        // Si se ha llegado a result_izq, se aprovecha que sabemos que la suma de result_izq es la mayor
-        if (lim_izq_tmp==result_izq.second){
-            sum_tmp += suma_izq;
-            if (sum_tmp>=sum_max){
-                result_central.first = result_izq.first;
-                sum_max = sum_tmp;
+        // Si se ha llegado a bounds_max_izq, se aprovecha que sabemos que la suma de bounds_max_izq es la mayor
+        if (lim_izq_tmp==bounds_max_izq.second){
+            suma_central_tmp += suma_max_izq;
+            if (suma_central_tmp>=suma_max_central){
+                bounds_max_central.first = bounds_max_izq.first;
+                suma_max_central = suma_central_tmp;
             }
         }
 		
 		// Actualizo el borde por la dcha.
-		// Voy desplazandome a la dcha (hasta encontrarme con result_dcha) y actualizando la suma
-		sum_tmp = sum_max;
+		// Voy desplazandome a la dcha (hasta encontrarme con bounds_max_dcha) y actualizando la suma
+		suma_central_tmp = suma_max_central;
         int lim_dcha_tmp;
-		for (lim_dcha_tmp=result_central.second+1; lim_dcha_tmp<result_dcha.first; lim_dcha_tmp++){
-			sum_tmp += v[lim_dcha_tmp];
-			if (sum_tmp>=sum_max){
-				result_central.second = lim_dcha_tmp;
-				sum_max = sum_tmp;
+		for (lim_dcha_tmp=bounds_max_central.second+1; lim_dcha_tmp<bounds_max_dcha.first; lim_dcha_tmp++){
+			suma_central_tmp += v[lim_dcha_tmp];
+			if (suma_central_tmp>=suma_max_central){
+				bounds_max_central.second = lim_dcha_tmp;
+				suma_max_central = suma_central_tmp;
 			}
 		}
-        // Si se ha llegado a result_dcha, se aprovecha que sabemos que la suma de result_dcha es la mayor
-        if (lim_dcha_tmp==result_dcha.first){
-            sum_tmp += suma_dcha;
-            if (sum_tmp>=sum_max){
-                result_central.second = result_dcha.second;
-                sum_max = sum_tmp;
+        // Si se ha llegado a bounds_max_dcha, se aprovecha que sabemos que la suma de bounds_max_dcha es la mayor
+        if (lim_dcha_tmp==bounds_max_dcha.first){
+            suma_central_tmp += suma_max_dcha;
+            if (suma_central_tmp>=suma_max_central){
+                bounds_max_central.second = bounds_max_dcha.second;
+                suma_max_central = suma_central_tmp;
             }
         }
 
-        // Se actualiza la suma de la subsecuencia central
-        int suma_central = sum_max;
-
 
         //Combinacion de casos. Me quedo con el caso mejor.
-        if(suma_izq>=suma_dcha){
-            result = result_izq;
-            suma_result=suma_izq;
+        if(suma_max_izq>=suma_max_dcha){
+            bounds_max = bounds_max_izq;
+            suma_max = suma_max_izq;
         }
         else{
-            result = result_dcha;
-            suma_result=suma_dcha;
+            bounds_max = bounds_max_dcha;
+            suma_max = suma_max_dcha;
         }
 
-        if(suma_central>=suma_result){
-            result = result_central;
-            suma_result=suma_central;
+        if(suma_max_central>= suma_max){
+            bounds_max = bounds_max_central;
+            suma_max = suma_max_central;
         }
 
         #ifdef DEBUG
         // Imprimo los resultados parciales, para comprobar que el algoritmo funciona correctamente
         cout << "Init: " << init << " Fin: " << fin << endl;
         cout << "Secuencia: ";
-        for(int i=init; i<=fin; i++)
+        for(int i=init; i<=fin; ++i)
             cout << v[i] << " ";
         cout << endl;
-        cout << "Izq: " << result_izq.first << " " << result_izq.second << " " << suma_izq << endl;
-        cout << "Dcha: " << result_dcha.first << " " << result_dcha.second << " " << suma_dcha << endl;
-        cout << "Central: " << result_central.first << " " << result_central.second << " " << suma_central << endl;
-        cout << "Mejor: " << result.first << " " << result.second << " " << suma_result << endl;
+        cout << "Izq: " << bounds_max_izq.first << " " << bounds_max_izq.second << " " << suma_max_izq << endl;
+        cout << "Dcha: " << bounds_max_dcha.first << " " << bounds_max_dcha.second << " " << suma_max_dcha << endl;
+        cout << "Central: " << bounds_max_central.first << " " << bounds_max_central.second << " " << suma_central << endl;
+        cout << "Mejor: " << result.first << " " << result.second << " " <<  suma_max << endl;
         cout << endl;
         #endif
-
-        return result;
     }
+
+    // Si la suma maxima es negativa, devolvemos un par de indices invalidos
+    if (suma_max<0){
+        const int INVALID_INDEX = -1;
+        return pair<int,int>(INVALID_INDEX,INVALID_INDEX);
+    }
+    else
+        return bounds_max;
 }
 
 
@@ -172,7 +182,7 @@ int main (int argc,char **argv){
 
         tantes= chrono::high_resolution_clock::now();
 
-        result = subsecMax(v,0,v.size()-1);
+        result = subsecMax_DyV(v,0,v.size()-1);
 
         tdespues = chrono::high_resolution_clock::now();
 
--- P2/programa1/Subsecuencia_Iterativo.cpp
+++ P2/programa1/Subsecuencia_Iterativo.cpp
@@ -1,65 +1,90 @@
 #include <iostream>
 #include <random>
-#include <time.h>
 #include <utility>
+#include <vector>
+
+using namespace std;
 
 
+/**
+ * @brief Funcion que calcula la suma de los elementos de un vector en un rango determinado.
+ * 
+ * @param v  Vector de enteros
+ * @param init Posicion inicial
+ * @param fin  Posicion final
+ * @return int Suma de los elementos del vector en el rango [init,fin]
+ */
+int suma(const vector<int> & v, int init, int fin){
+    int result=0;
 
-int SumaMaximaNum(int* arr, int n){
-    int max=0, localmax=0;
-    int* endpos = arr + n;
-    for(int* pos = arr; pos < endpos; ++pos){
-        int elem = *pos;
-        if(localmax > 0){
-            localmax += elem;
-            
-        } else{
-            localmax = elem;
-        }
-        max = std::max(max, localmax);
-        
-    }
-    return std::max(max,0);
+    bool index_ok = 0<=init && init<=fin && fin<v.size();
+
+    // Tan solo se calcula la suma si los indices son correctos
+    // Evitamos así las subsecuencias vacias
+    if(index_ok)
+        for(int i=init; i<=fin; ++i)
+            result+=v[i];
+    
+    return result;
 }
 
-std::pair<int,int> SumaMaximaPos(int* arr, int n){
-    int max=0, localmax=0;
-    std::pair<int,int> bounds(0,0);
-    int localini=0, localfin=0;
-    for(int i=0; i < n; ++i){
+
+/**
+ * @brief Funcion que calcula la suma maxima de una subsecuencia de un vector de enteros. Algoritmo Iterativo.
+ * 
+ * @param v Vector de enteros
+ * @param init  Posicion inicial
+ * @param fin  Posicion final
+ * @return pair<int,int>  Par de enteros que representan la posicion inicial y final de la subsecuencia maxima
+ */
+pair<int,int> subsecMax_Itertivo (const vector<int> &v, int init, int fin){
+
+    // Variables para almacenar la subsecuencia maxima local
+    int localini=0, localfin=0, localmax=0;
+
+    int max=0; // Variable para almacenar la suma de la subsecuencia maxima por ahora
+    const int INVALID_INDEX = -1; // Valor para indicar que no se ha encontrado una subsecuencia valida
+    pair<int,int> bounds_max(INVALID_INDEX,INVALID_INDEX); // Limites de la subsecuencia maxima
+
+    
+    for(int i=init; i<fin; ++i){
         
-        if(localmax > 0){
-            localmax += arr[i];
+        // Si la suma local es negativa, no la consideramos porque nos quedaremos con la subsecuencia vacia
+        if(localmax >= 0){
+            // Se actualiza la suma local y el límite de la derecha avanza
+            localmax += v[i];
             localfin = i;
         } else{
-            localmax = arr[i];
+            // La subsecuencia anterior no era válida. La reseteamos a la subsecuencia que empieza en i 
+            localmax = v[i];
             localini = localfin = i;
         }
-        //Si se quiere la subsecuencia mas pequeña posible, añadir:
-        // || (localmax == max && localfin - localini < bounds.second - bounds.first)
+
+
+        // Actualizamos la subsecuencia maxima si la suma local es mayor
+                //Si se quiere la subsecuencia mas pequeña posible, añadir:
+                // || (localmax == max && localfin - localini < bounds_max.second - bounds_max.first)
         if (localmax > max){ 
             max = localmax;
-            bounds.first = localini;
-            bounds.second = localfin;
+            bounds_max.first = localini;
+            bounds_max.second = localfin;
         }
         
     }
-    return bounds;
+    return bounds_max;
 
 }
 
 int main(){
-    srand((unsigned int)time(NULL));
-    int n = 16;
-    int arr[n] = {1,2,40,-50,7,-10,4,20,  30,-11,-1,0,0,0,-200,40};
+    const vector<int> v = {1,2,40,-50,7,-10,4,20,  30,-11,-1,0,0,0,-200,40};
 
 
-    for(int i=0; i<n; ++i){
-        std::cout << arr[i] << " ";
+    for(int i=0; i<v.size(); ++i){
+        cout << v[i] << " ";
     }
-    std::cout << std::endl;
+    cout << endl;
 
-    std::pair<int,int> bounds = SumaMaximaPos(arr,n);
-    std::cout << SumaMaximaNum(arr, n) << "   " << bounds.first << " " << bounds.second << std::endl;
+    pair<int,int> bounds_max = subsecMax_Itertivo(v,0, v.size()-1);
+    cout << suma(v, bounds_max.first, bounds_max.second) << "   " << bounds_max.first << " " << bounds_max.second << endl;
     return 0;
 }
