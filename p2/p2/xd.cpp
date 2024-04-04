bec5ebe96a6e73cc76e0719a44243e1412831ce5
arturo-olvrs <2004.olivares@gmail.com>
2024-04-04 18:39:12 +0200

    makefiles generados

--- p2/p2/xd.cpp
+++
@@ -1,5 +0,0 @@
-
-    // Enlosar(M,n-1,row*!( lower  &&  right ),col*!( lower  &&  right ),first_row,first_col);
-    // Enlosar(M,n-1,row* (!lower  &&  right ),col* (!lower  &&  right ),first_row+middle,first_col);
-    // Enlosar(M,n-1,row* ( lower  && !right ),col* ( lower  && !right ),first_row,first_col+middle);
-    // Enlosar(M,n-1,row* ( lower  &&  right ),col* ( lower  &&  right ),first_row+middle,first_col+middle);
