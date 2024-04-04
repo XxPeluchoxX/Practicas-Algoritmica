39ce273a3392937853ef8750683f49336b5f324e
irinaa0021 <uwu.git.infomates@gmail.com>
2024-04-04 09:02:53 +0200

    Arreglado el 3

--- P2/programa3/ViajanteDelComercio_Iterativo.cpp
+++ P2/programa3/ViajanteDelComercio_Iterativo.cpp
@@ -41,6 +41,7 @@ void mejor_camino_Iterativo(vector<pair<int,int>> &v, int inf, int sup){
     if (inf >= sup){ // Caso base, no hay ciudades que considerar
         return;
     }
+    sort(v.begin()+1, v.end()); // El next_permutation necesita que este el vector en orden ascendente.
 
     int n = sup - inf + 1;  // Número de ciudades que consideramos
     double min = DBL_MAX;   // Distancia mínima inicial. La inicializamos a un valor muy grande para que cualquier distancia sea menor
